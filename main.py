f34114d29b71a0350f9a8a0066fe49508a324d91
tommahawk25 <seky.tomas@gmail.com>
2022-12-21 21:22:09 +0100

    minor changes (comments, text, variable name, dictionary visual structure)

--- main.py
+++ main.py
@@ -1,52 +1,20 @@
 alphabet = {
-    ".-": "A",
-    "-...": "B",
-    "-.-.": "C",
-    "-..": "D",
-    ".": "E",
-    "..-.": "F",
-    "--.": "G",
-    "....": "H",
-    "----": "CH",
-    "..": "I",
-    ".---": "J",
-    "-.-": "K",
-    ".-..": "L",
-    "--": "M",
-    "-.": "N",
-    "---": "O",
-    ".--.": "P",
-    "--.-": "Q",
-    ".-.": "R",
-    "...": "S",
-    "-": "T",
-    "..-": "U",
-    "...-": "V",
-    ".--": "W",
-    "-..-": "X",
-    "-.--": "Y",
-    "--..": "Z",
-    ".----": "1",
-    "..---": "2",
-    "...--": "3",
-    "....-": "4",
-    ".....": "5",
-    "-....": "6",
-    "--...": "7",
-    "---..": "8",
-    "----.": "9",
-    "-----": "0",
-    "..--..": "?",
-    "--..--": ",",
-    "--...-": "!",
-    ".-.-.-": ".",
-    ".--.-.": "@",
-    "-..-.": "/",
-    "-....-": "-",
-    "-.--.": "(",
-    "-.--.-": ")",
-    ".----.": "'",
-    ".-..-.": "\""
+    ".-": "A", "-...": "B", "-.-.": "C",
+    "-..": "D", ".": "E", "..-.": "F",
+    "--.": "G", "....": "H", "----": "CH",
+    "..": "I", ".---": "J", "-.-": "K",
+    ".-..": "L", "--": "M", "-.": "N",
+    "---": "O", ".--.": "P", "--.-": "Q",
+    ".-.": "R", "...": "S", "-": "T",
+    "..-": "U", "...-": "V", ".--": "W",
+    "-..-": "X", "-.--": "Y", "--..": "Z",
+    ".----": "1", "..---": "2", "...--": "3",
+    "....-": "4", ".....": "5", "-....": "6",
+    "--...": "7", "---..": "8", "----.": "9",
+    "-----": "0", "..--..": "?", "--..--": ",",
+    "--...-": "!", ".-.-.-": ".", ".--.-.": "@",
+    "-..-.": "/", "-....-": "-", "-.--.": "(",
+    "-.--.-": ")", ".----.": "'", ".-..-.": "\""
 }
 # possible to add more symbols if needed
 
@@ -59,10 +27,10 @@ def translate_to_text():
         letter = ""
         count = 0
         spaces = morse_code.count(" ")
-        backslashes = morse_code.count("//")
+        double_slashes = morse_code.count("//")
 
         # morse code format 1 (spaces between letters are " " and spaces between words are " / ")
-        if spaces > 0 and backslashes == 0:
+        if spaces > 0 and double_slashes == 0:
             for symbol in morse_code:
                 # count iterations
                 count += 1
@@ -75,13 +43,13 @@ def translate_to_text():
                     translation += letter + " "
                     # clear temporary result for another iteration
                     temp_result = ""
-                # if symbol is "/", then it is space taken care of in previous iteration
+                # if symbol is "/", then it is space and is taken care of in previous iteration
                 elif symbol == "/":
                     pass
-                # if symbol is " " and previous symbol is "/", then it is space taken care of in previous iteration
+                # if symbol is " " and previous symbol is "/", then space is taken care of in previous iteration
                 elif symbol == " " and morse_code[count-2] == "/":
                     pass
-                # if it is " ", then letter is done and is translated
+                # if it is " ", then letter is completed and is translated
                 else:
                     letter = alphabet.get(temp_result)
                     translation += letter
@@ -102,7 +70,8 @@ def translate_to_text():
                     # if previous symbol is also "/" or "|", then it is space between words
                     if morse_code[count - 2] == "/" or morse_code[count - 2] == "|":
                         translation += " "
-                    # if previous symbol is not "/" nor "|", then it is space between letter, letter is translated
+                    # if previous symbol is not "/" nor "|" then it is space between letters (taken care of already)
+                    # and letter is completed and translated
                     else:
                         letter = alphabet.get(temp_result)
                         translation += letter
@@ -129,6 +98,7 @@ def translate_to_morse():
         for symbol in text:
             symbol = symbol.upper()
             if symbol != " ":
+                # get key by value in dictionary
                 translation += list(alphabet.keys())[list(alphabet.values()).index(symbol)] + "/"
             else:
                 translation += "//"
@@ -141,13 +111,13 @@ def translate_to_morse():
 
 
 def user_action():
-    action = input("1. Text to Morse Code\n2. Morse Code to text\nType number of action: ")
+    action = input("1. Text to Morse Code\n2. Morse Code to text\nEnter number of action: ")
     if action == "1":
         translate_to_morse()
     elif action == "2":
         translate_to_text()
     else:
-        print("Type number 1 or 2!")
+        print("Enter number 1 or 2!")
         user_action()
 
 
